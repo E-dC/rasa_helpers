ad35a19b2c53abf816de02f70f178082464d3100
EdC <22207434+E-dC@users.noreply.github.com>
2021-11-16 17:47:31 +0100

    Add NLU functionality to library: allowing custom code to switch between models, running models.

---
+++ rasa_helpers/nlu.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import importlib
+import ruamel.yaml as yaml
+import rasa.model
+import rasa.nlu.model
+from sanic.log import logger
+from .base_updater import AppUpdater
+
+class NLUAppUpdater(AppUpdater):
+
+    @classmethod
+    def _load_updated_data(cls, filename):
+        """Load Rasa NLU model from a file.
+
+            Args:
+                filename (str): Model filename
+
+            Returns:
+                rasa.nlu.model.Interpreter loaded
+        """
+        m = rasa.model.get_model(filename)
+        return rasa.nlu.model.Interpreter.load(
+            os.path.join(m, 'nlu')
+        )
+
+    @classmethod
+    def _load_chooser_code(cls, filepath, fname):
+        """ Load user code to switch between NLU models.
+
+            Details:
+                If the config is:
+
+                NLU_CONTROLS:
+                    MODEL_CHOOSER:
+                        FILEPATH: 'chooser_code.py'
+                        FUNCTION: 'chooser'
+                    VALUES:
+                        - NAME: eng
+                          FILENAME: ....
+                        - NAME: fra
+                          FILENAME: ....
+
+                Then the user must be implement a function `chooser` in
+                `chooser_code.py`, and `chooser` must take some text as an input,
+                and returns a tuple (label, confidence) where
+                label is either 'eng' or 'fra', and confidence is a float.
+                These labels will allow the server to choose the suitable NLU model.
+                The confidence is included for possible future improvements.
+
+            Args:
+                filepath (str): Path to the source code to load
+                fname (str): Name of the function to use for switching
+                    between models.
+
+            Returns:
+                a function
+
+        """
+        module_name = 'user_chooser'
+        spec = importlib.util.spec_from_file_location(module_name, filepath)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+
+        return sys.modules[module_name].__dict__[fname]
+
+
+    @classmethod
+    def refresh(cls, app):
+        """ Update the app responses if a newer version is available.
+
+            Details:
+                `app` MUST have been configured once beforehand.
+                `app` is modified in-place.
+                `MODELS` field of app config will look like:
+                    switching_value1: loaded interpreter for switching_value1
+                    switching_value2: loaded interpreter for switching_value2
+
+            Args:
+                app (sanic.Sanic): Sanic app to configure
+
+            Returns:
+                None
+        """
+
+        updated = cls._base_refresh(app, caller='nlu')
+        return None
+
+    @classmethod
+    def configure(cls, app, config_filename):
+        """ Setup the app when first starting the NLU server.
+
+            Details:
+                `app` is modified in-place.
+                The config settings are stored in `app.config`.
+
+            Args:
+                app (sanic.Sanic): Sanic app to configure
+                config_filename (str): NLU config to use
+
+            Returns:
+                None
+
+        """
+        cls._base_configure(app, config_filename, caller='nlu')
+
+        app.config['MODELS'] = {}
+        app.config.NLU_REFRESH = app.config.NLU_CONTROLS['REFRESH']
+        app.config.NLU_CHOOSER = cls._load_chooser_code(
+            app.config.NLU_CONTROLS['MODEL_CHOOSER']['FILEPATH'],
+            app.config.NLU_CONTROLS['MODEL_CHOOSER']['FUNCTION']
+        )
+
+        cls.refresh(app)
+
+        return None
+
+class NLURunner(object):
+
+    @classmethod
+    def _unpack_request(cls, request):
+        return request.json['text']
+
+    @classmethod
+    def _amend_response(cls, app, response, label, confidence):
+        response['entities'].append({
+            'start': 0,
+            'end': 0,
+            'value': label,
+            'entity': app.config['NLU_CONTROLS']['NAME'],
+            'confidence': confidence
+        })
+        return response
+
+    @classmethod
+    def run_chooser(cls, app, message):
+        o = app.config['NLU_CHOOSER'](message)
+        try:
+            assert o[0] in app.config.MODELS.keys()
+        except AssertionError:
+            logger.error(
+                f'{o[0]} is not in the allowable labels {app.config.MODELS.keys()}')
+            raise AssertionError
+        return o
+
+    @classmethod
+    def run_intent_classification(cls, app, label, message):
+        return app.config.MODELS[label].parse(message)
+
+    @classmethod
+    def run(cls, app, request):
+        message = cls._unpack_request(request)
+        label, confidence = cls.run_chooser(app, message)
+        response = cls.run_intent_classification(app, label, message)
+        response = cls._amend_response(app, response, label, confidence)
+        return response
