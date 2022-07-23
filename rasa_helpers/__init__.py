0d346f13bd99f074592ecb584a55ccefc4e47f74
EdC <22207434+E-dC@users.noreply.github.com>
2022-07-23 14:15:40 +0300

    style: remove commented out code

--- rasa_helpers/__init__.py
+++ rasa_helpers/__init__.py
@@ -1,6 +1,2 @@
 __doc__ = """rasa_helpers is a collection of tools useful for the development
 and testing of conversational agents based on Rasa"""
-
-# from .tracker_builder import TestTrackerStore
-#
-# from .nlg import NLGLoader
